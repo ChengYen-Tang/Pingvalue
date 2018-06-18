294c4ad9211e7955be0c29ad12df470bc4ca2da1
唐振嚴 <kenneth@gm.nssh.ntpc.edu.tw>
2018-06-18 20:19:58 +0800

    加入專案檔案。

---
+++ Pingvalue/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pingvalue.Startup))]
+namespace Pingvalue
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
