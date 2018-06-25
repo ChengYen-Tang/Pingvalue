a2a932a574243620b85c08183f46a9d22c8e37b6
唐振嚴 <kenneth@gm.nssh.ntpc.edu.tw>
2018-06-26 00:31:51 +0800

    Create device models

---
+++ Pingvalue/Models/DeviceModels.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pingvalue.Models
+{
+    public partial class DeviceGroup
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public DeviceGroup()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreateTime = DateTime.Now;
+
+            this.Devices = new List<Device>();
+        }
+
+        public ICollection<Device> Devices { get; set; }
+    }
+
+    public partial class Device
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        public string DeviceName { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string IPAddress { get; set; }
+        [Required]
+        public bool IsOnline { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public Device()
+        {
+            this.Id = new Guid();
+            this.CreateTime = DateTime.Now;
+
+            this.DeviceGroups = new List<DeviceGroup>();
+            this.pingDatas = new List<PingData>();
+        }
+
+        public ICollection<DeviceGroup> DeviceGroups { get; set; }
+        public ICollection<PingData> pingDatas { get; set; }
+    }
+
+    public partial class PingData
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        public long Delay1 { get; set; }
+        public long Delay2 { get; set; }
+        public long Delay3 { get; set; }
+        public long Delay4 { get; set; }
+
+        public PingData()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreateTime = DateTime.Now;
+        }
+
+        public Device Device { get; set; }
+    }
+}
---
+++ Pingvalue/Models/PingvalueModels.cs
@@ -0,0 +1,24 @@
+namespace Pingvalue.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class PingvalueModels : DbContext
+    {
+        public PingvalueModels()
+            : base("name=DefaultConnection")
+        {
+        }
+
+        public virtual DbSet<DeviceGroup> DeviceGroups { get; set; }
+        public virtual DbSet<Device> Devices { get; set; }
+        public virtual DbSet<PingData> PingDatas { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
