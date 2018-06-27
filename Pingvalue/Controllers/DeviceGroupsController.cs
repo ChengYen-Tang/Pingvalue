28be37d8aa620a0a2ddac238d033a82f1eb81d68
唐振嚴 <kenneth@gm.nssh.ntpc.edu.tw>
2018-06-27 23:57:44 +0800

    產生Devices、DeviceGroups、PingDatas相關頁面

---
+++ Pingvalue/Controllers/DeviceGroupsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pingvalue.Models;
+
+namespace Pingvalue.Controllers
+{
+    public class DeviceGroupsController : Controller
+    {
+        private PingvalueModels db = new PingvalueModels();
+
+        // GET: DeviceGroups
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.DeviceGroups.ToListAsync());
+        }
+
+        // GET: DeviceGroups/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeviceGroup deviceGroup = await db.DeviceGroups.FindAsync(id);
+            if (deviceGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deviceGroup);
+        }
+
+        // GET: DeviceGroups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DeviceGroups/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,GroupName,CreateTime")] DeviceGroup deviceGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                deviceGroup.Id = Guid.NewGuid();
+                db.DeviceGroups.Add(deviceGroup);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(deviceGroup);
+        }
+
+        // GET: DeviceGroups/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeviceGroup deviceGroup = await db.DeviceGroups.FindAsync(id);
+            if (deviceGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deviceGroup);
+        }
+
+        // POST: DeviceGroups/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,GroupName,CreateTime")] DeviceGroup deviceGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deviceGroup).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(deviceGroup);
+        }
+
+        // GET: DeviceGroups/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeviceGroup deviceGroup = await db.DeviceGroups.FindAsync(id);
+            if (deviceGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deviceGroup);
+        }
+
+        // POST: DeviceGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            DeviceGroup deviceGroup = await db.DeviceGroups.FindAsync(id);
+            db.DeviceGroups.Remove(deviceGroup);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Pingvalue/Controllers/DevicesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pingvalue.Models;
+
+namespace Pingvalue.Controllers
+{
+    public class DevicesController : Controller
+    {
+        private PingvalueModels db = new PingvalueModels();
+
+        // GET: Devices
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Devices.ToListAsync());
+        }
+
+        // GET: Devices/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = await db.Devices.FindAsync(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // GET: Devices/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Devices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,DeviceName,IPAddress,IsOnline,CreateTime")] Device device)
+        {
+            if (ModelState.IsValid)
+            {
+                device.Id = Guid.NewGuid();
+                db.Devices.Add(device);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(device);
+        }
+
+        // GET: Devices/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = await db.Devices.FindAsync(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: Devices/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,DeviceName,IPAddress,IsOnline,CreateTime")] Device device)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(device).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(device);
+        }
+
+        // GET: Devices/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = await db.Devices.FindAsync(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: Devices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            Device device = await db.Devices.FindAsync(id);
+            db.Devices.Remove(device);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
